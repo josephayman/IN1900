d07eff23a1e30a5291e3da18e4a9f9a2e7303aa0
Joseph Ayman <josephayman@mail.com>
2022-11-20 17:44:14 +0100

    Solution

--- Uke 3/sum_for.py
+++ Uke 3/sum_for.py
@@ -1,5 +1,19 @@
 s = 0
 M = 3
-for k in range(M):
+for k in range(1, M+1): 
+    s += 1/(2*k**2)
+print(s)
+
+"""
+Wrong code
+s = 0
+M = 3
+for i in range(M):
     s += 1/2*k**2
 print(s)
+
+1) The variable k is not defined in the loop. So we change i to k.
+2) The loop is not executed M times, but M-1 times. So we change range(M) to range(1, M+1).
+3) The formula is wrong. We should use parentheses to make sure that the exponentiation is done first.
+
+"""
